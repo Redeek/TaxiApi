7f046cfe45eb71bd8e2e5e7eb88c38bd7786e4fb
Redeek <mati.red@gmail.com>
2025-04-17 18:34:33 +0200

    check push on master branch

--- TaxiApi/Models/RegisterUserDto.cs
+++ TaxiApi/Models/RegisterUserDto.cs
@@ -8,7 +8,6 @@ namespace TaxiApi.Models
         [Required]
         public string Email { get; set; }
 
-        [Required]
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
         public string Name { get; set; }
