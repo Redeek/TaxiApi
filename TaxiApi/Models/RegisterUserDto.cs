eac5f947168a26b777f49453d2b03679af1f343d
Redeek <mati.red@gmail.com>
2025-04-17 18:27:59 +0200

    fix code

--- TaxiApi/Models/RegisterUserDto.cs
+++ TaxiApi/Models/RegisterUserDto.cs
@@ -8,6 +8,7 @@ namespace TaxiApi.Models
         [Required]
         public string Email { get; set; }
 
+        [Required]
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
         public string Name { get; set; }
