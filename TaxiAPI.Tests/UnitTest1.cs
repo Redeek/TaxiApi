e95d7afcc124f7ffc2775a2e1129d17fe81e8cf7
Redeek <mati.red@gmail.com>
2025-04-26 19:54:13 +0200

    Test delete user service

--- TaxiAPI.Tests/UnitTest1.cs
+++ TaxiAPI.Tests/UnitTest1.cs
@@ -1,71 +1,85 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using MockQueryable.Moq;
-using Moq;
 using TaxiApi.Entities;
+using TaxiApi.Exceptions;
 using TaxiApi.Models;
 using TaxiApi.Services;
 
 namespace TaxiAPI.Tests
 {
-    public class DriverServiceTests
+    public class AccountServiceTests
     {
-        private readonly Mock<TaxiDbContext> _dbContextMock;
-        private readonly Mock<IMapper> _mockMapper;
+        private AccountService _service;
+        private TaxiDbContext _context;
 
-        private readonly DriverService _driverService;
-        private readonly Mock<ILogger> _mockLogger;
-
-        public DriverServiceTests()
+        public AccountServiceTests()
         {
-            _dbContextMock = new Mock<TaxiDbContext>();
-            _mockMapper = new Mock<IMapper>();
-            _mockLogger = new Mock<ILogger>();
+            var opt = new DbContextOptionsBuilder<TaxiDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
 
-            _driverService = new DriverService(_dbContextMock.Object, _mockMapper.Object);
+            _context = new TaxiDbContext(opt);
+            _service = new AccountService(_context, null, null, null);
+
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+        }
+
+        private User CreateTestUser(int id, int roleId)
+        {
+            return new User
+            {
+                Id = id,
+                RoleId = roleId,
+                Email = $"test{id}@example.com",
+                Name = "TestName",
+                Password = "TestPassword",
+                PhoneNumber = "123456789",
+                Surname = "TestSurname"
+            };
+        }
+
+
+        [Fact]
+        public void DeleteUser_UserNotFound_ThrowsNotFoundException()
+        {
+            int nonExistingUserId = 9999;
+
+            Assert.Throws<NotFoundException>(() => _service.DeleteUser(nonExistingUserId));
         }
 
         [Fact]
-        public void AssignDriverToCar_ShouldAssignCarToDriver_WhenDataIsValid()
+        public void DeleteUser_UserNotDriver_ThrowsUserInvalidOperationException()
         {
-            var driverId = 1;
-            var carId = 2;
+            var user = CreateTestUser(1,2);
+            _context.Users.Add(user);
+            _context.SaveChanges();
 
-            var car = new Car { Id = carId };
-            var driver = new Driver { Id = driverId, Cars = new List<Car>() };
 
-            var cars = new List<Car> { car }.AsQueryable().BuildMockDbSet();
-            var drivers = new List<Driver> { driver }.AsQueryable().BuildMockDbSet();
-
-            _dbContextMock.Setup(db => db.Cars).Returns(cars.Object);
-            _dbContextMock.Setup(db => db.Drivers).Returns(drivers.Object);
-            _dbContextMock.Setup(db => db.SaveChanges()).Verifiable();
-
-            var service = new DriverService(_dbContextMock.Object, _mockMapper.Object);
-
-            service.AssignDriverToCar(driverId, carId);
-
-            // Assert
-            Assert.Single(driver.Cars);
-            Assert.Equal(carId, driver.Cars.First().Id);
-            _dbContextMock.Verify(c => c.SaveChanges(), Times.Once);
+            Assert.Throws<UserInvalidOperationException>(() => _service.DeleteUser(user.Id));
         }
+
+        [Fact]
+        public void DeleteUser_ValidDriver_DeleteUser()
+        {
+            var user = CreateTestUser(1,1);
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+
+            _service.DeleteUser(user.Id);
+
+            var deletedUser = _context.Users.FirstOrDefault(u => u.Id == user.Id);
+            Assert.Null(deletedUser);
+        }
+
 
 
 
     }
 
-    public static class MockQueryableExtensions
-    {
-        public static Mock<DbSet<T>> BuildMockDbSet<T>(this IQueryable<T> source) where T : class
-        {
-            var mock = new Mock<DbSet<T>>();
-            mock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(source.Provider);
-            mock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(source.Expression);
-            mock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(source.ElementType);
-            mock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => source.GetEnumerator());
-            return mock;
-        }
-    }
+
 }
+
